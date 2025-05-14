dd1cb5a471de7075202c0ca8af9b5a0b3e6064ea
Ado <adonaylester85@gmail.com>
2025-05-14 15:47:58 -0600

    Update Game•acertijo.js

--- plugins/Game•acertijo.js
+++ plugins/Game•acertijo.js
@@ -1,32 +1,40 @@
 import fs from 'fs';
+
+
 const timeout = 60000;
-const poin = 10;
+const poin = 500;
 const handler = async (m, {conn, usedPrefix}) => {
+
   conn.tekateki = conn.tekateki ? conn.tekateki : {};
   const id = m.chat;
   if (id in conn.tekateki) {
-    conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0]);
+    conn.reply(m.chat, '*[ ℹ️ ] Estas En Juego*' , conn.tekateki[id][0]);
     throw false;
   }
-  const tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`));
+
+const filePath = './src/acertijo.json';
+const fileContent = fs.readFileSync(filePath, 'utf8');
+const tekateki = JSON.parse(fileContent);
+
   const json = tekateki[Math.floor(Math.random() * tekateki.length)];
   const _clue = json.response;
   const clue = _clue.replace(/[A-Za-z]/g, '_');
-  const caption = `
-ⷮ🚩 *ACERTIJOS*
-✨️ *${json.question}*
 
-⏱️ *Tiempo:* ${(timeout / 1000).toFixed(2)} Segundos
-🎁 *Premio:* *+${poin}* Centavos 🪙`.trim();
+  const caption = `
+ⷮ *${json.question}* 
+• *Tiempo restante:* ${(timeout / 1000).toFixed(2)} segundos
+• *Puntos:* +${poin} Exp
+`.trim();
+
   conn.tekateki[id] = [
     await conn.reply(m.chat, caption, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `🤍 Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
+      if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo! La respuesta era: ${json.response}`, conn.tekateki[id][0]);
       delete conn.tekateki[id];
     }, timeout)];
 };
 handler.help = ['acertijo'];
-handler.tags = ['fun'];
-handler.command = ['acertijo', 'acert', 'adivinanza', 'tekateki'];
+handler.tags = ['game'];
+handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i;
 export default handler;
