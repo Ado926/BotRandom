303a2a239c03ab7ccc26b44f128dbce39dd156a9
Ado <adonaylester85@gmail.com>
2025-05-15 14:34:13 -0600

    Update Convertidor•tourl.js

--- plugins/Convertidor•tourl.js
+++ plugins/Convertidor•tourl.js
@@ -23,7 +23,7 @@ let handler = async (m) => {
       txt += `› Acortado : ${await shortUrl(link)}\n`
       txt += `› Tamaño : ${formatBytes(media.length)}\n`
       txt += `› Expiración : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
-      txt += `> *🌵 Desarrollado por Daniel*`
+      txt += `> *💚 Desarrollado por Wirk*`
 
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
 await m.react(done)
